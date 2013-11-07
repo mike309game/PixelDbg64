fc3a0f5f86ba207b7bf77384e1ab4bf142199751
N.Kindt <n.kindt@n.kindt>
2013-11-07 12:14:09 +0100

    + Added notes on how to compile with MinGW. + Added compile batch file.

---
+++ make.bat
@@ -0,0 +1,7 @@
+echo off
+cls
+
+set arg1=%1
+set arg2=%2
+windres pdbg.rc -O coff -o pdbg.res
+g++ main.cpp -o PixelDbg.exe -march=i686 -mwindows -s -O2 -I%arg1% -Wint-to-pointer-cast -L%arg2% -lfltk -lgdi32 -lcomctl32 -lole32 -luuid -lcomdlg32 pdbg.res
